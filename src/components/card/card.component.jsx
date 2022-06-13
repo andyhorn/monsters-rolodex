21680ca45fa50454fd00120b38bcc84362bb961c
Andrew Horn <andyjhorn@gmail.com>
2022-06-13 11:25:18 -0500

    fixed typo

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,9 +1,8 @@
 import "./card.styles.css";
 
 const Card = ({ monster }) => {
-    console.log(monster)
     const { email, id, name } = monster;
-    const imgSrc = `https://robohash.org/${id}?set=set2&size=180.180`;
+    const imgSrc = `https://robohash.org/${id}?set=set2&size=180x180`;
 
     return (
         <div className="card-container">
